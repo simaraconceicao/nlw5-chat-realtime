f9ed755c6d079c3452ab850b65bd94883d278920
simaraconceicao <contato@simaraconceicao.com>
2021-04-20 13:12:18 -0300

    cria conexao com bd sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from 'express';
+import "./database";
+import { routes } from './routes';
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json({
-       message: "Olá NLW 05!"
-    });
-});
-
-app.post("/users", (req, res) => {
-    return res.json({
-        message: "Usuário salvo com sucesso!"
-    });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
 
